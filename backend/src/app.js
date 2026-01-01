0862dd8cda6aed5cc79ba75466e8bb3673bf72da
youknowhim <pallavrai39@gmail.com>
2026-01-01 22:42:48 +0530

    added all app.js logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+// Parse JSON request bodies
 app.use(express.json());
 
-// Simple health check
+
+// User creation, stats & analytics (main dashboard)
+app.use("/user", require("./routes/user.routes"));
+
+// Global leaderboard
+app.use("/", require("./routes/leaderboard.routes"));
+
+// Health check
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
 });
