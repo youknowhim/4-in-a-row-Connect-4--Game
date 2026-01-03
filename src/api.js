0799460c30757d413ec7776142b1a7fb13a9641e
youknowhim <pallavrai39@gmail.com>
2026-01-03 20:06:10 +0530

    added api.js for fetching backend apis

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const API_BASE = "http://localhost:1015";
+
+export async function createUser(username) {
+  const res = await fetch(`${API_BASE}/user`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username })
+  });
+
+  return res.json();
+}
+
+export async function getLeaderboard() {
+  const res = await fetch(`${API_BASE}/leaderboard`);
+  return res.json();
+}
