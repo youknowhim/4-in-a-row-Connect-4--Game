a11cb5e19d74df1f79cdb9041eeace979b9538ed
Pallav Rai <143748569+youknowhim@users.noreply.github.com>
2026-01-05 19:11:00 +0530

    Refactor Kafka producer initialization and usage

--- backend/src/kafka/producer.js
+++ backend/src/kafka/producer.js
@@ -1,48 +1,43 @@
-
 const { Kafka } = require("kafkajs");
 
-if (!process.env.KAFKA_BROKER) {
-  throw new Error("KAFKA_BROKER not defined");
-}
+const useKafka = process.env.USE_KAFKA === "true";
 
-const kafka = new Kafka({
-  clientId: "connect4-backend",
-  brokers: process.env.KAFKA_BROKER.split(","),
-
-  //AIVEN mTLS CONFIG
-ssl: {
-  rejectUnauthorized: true,
-  ca: process.env.KAFKA_CA_CERT.split("\\n"),
-  cert: process.env.KAFKA_ACCESS_CERT.replace(/\\n/g, "\n"),
-  key: process.env.KAFKA_ACCESS_KEY.replace(/\\n/g, "\n"),
-}
-
-});
-
-const producer = kafka.producer();
+let producer = null;
 
 async function initProducer() {
+  if (!useKafka) {
+    console.log("Kafka disabled (producer)");
+    return null;
+  }
+
+  const kafka = new Kafka({
+    clientId: "connect4-backend",
+    brokers: process.env.KAFKA_BROKER.split(","),
+    ssl: {
+      rejectUnauthorized: true,
+      ca: [process.env.KAFKA_CA_CERT],
+      cert: process.env.KAFKA_ACCESS_CERT,
+      key: process.env.KAFKA_ACCESS_KEY,
+    },
+  });
+
+  producer = kafka.producer();
   await producer.connect();
-  console.log("Kafka Producer connected (Aiven mTLS)");
+  console.log("Kafka Producer connected");
+  return producer;
 }
 
 async function publishGameFinished(payload) {
-  try {
-    await producer.send({
-      topic: "game-events",
-      messages: [
-        {
-          key: payload.gameId,
-          value: JSON.stringify(payload),
-        },
-      ],
-    });
-  } catch (err) {
-    console.error("Kafka publish failed:", err.message);
-  }
+  if (!useKafka) return; //
+
+  if (!producer) await initProducer();
+
+  await producer.send({
+    topic: "game-events",
+    messages: [{ value: JSON.stringify(payload) }],
+  });
 }
 
 module.exports = {
-  initProducer,
   publishGameFinished,
 };
