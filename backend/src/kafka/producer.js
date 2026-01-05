6c54522d01616663f744b0f73045fcb578139792
Pallav Rai <143748569+youknowhim@users.noreply.github.com>
2026-01-05 15:57:58 +0530

    Enhance Kafka producer with mTLS and error handling
    Updated error message for KAFKA_BROKER and added Aiven mTLS configuration for Kafka producer.

--- backend/src/kafka/producer.js
+++ backend/src/kafka/producer.js
@@ -1,19 +1,38 @@
+const fs = require("fs");
+const path = require("path");
 const { Kafka } = require("kafkajs");
 
 if (!process.env.KAFKA_BROKER) {
-  throw new Error("KAFKA_BROKERS not defined");
+  throw new Error("KAFKA_BROKER not defined");
 }
 
 const kafka = new Kafka({
   clientId: "connect4-backend",
   brokers: process.env.KAFKA_BROKER.split(","),
+
+  //AIVEN mTLS CONFIG
+  ssl: {
+    rejectUnauthorized: true,
+    key: fs.readFileSync(
+      path.join(__dirname, "../../", process.env.KAFKA_SSL_KEY),
+      "utf-8"
+    ),
+    cert: fs.readFileSync(
+      path.join(__dirname, "../../", process.env.KAFKA_SSL_CERT),
+      "utf-8"
+    ),
+    ca: fs.readFileSync(
+      path.join(__dirname, "../../", process.env.KAFKA_SSL_CA),
+      "utf-8"
+    ),
+  },
 });
 
 const producer = kafka.producer();
 
 async function initProducer() {
   await producer.connect();
-  console.log("✅ Kafka Producer connected");
+  console.log("Kafka Producer connected (Aiven mTLS)");
 }
 
 async function publishGameFinished(payload) {
